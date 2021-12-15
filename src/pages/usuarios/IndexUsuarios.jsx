c7d4adb3416feaf910716688c325642aebe08034
sebastianmora1618 <jeisonmora1618@gmail.com>
2021-12-14 19:35:00 -0500

    pagina de usuarios

--- src/pages/usuarios/IndexUsuarios.jsx
+++ src/pages/usuarios/IndexUsuarios.jsx
@@ -1,15 +1,63 @@
-import React, {useEffect} from 'react';
-import {useQuery} from '@apollo/client';
-import {GET_USUARIOS} from 'graphql/usuarios/querys';
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_USUARIOS } from 'graphql/usuarios/querys';
+import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+//import { Enum_Rol, Enum_EstadoUsuario } from 'utils/enums';
+//import PrivateRoute from 'components/PrivateRoute';
 
 const IndexUsuarios = () => {
-    const {data, error, loading} = useQuery(GET_USUARIOS);
+  const { data, error, loading } = useQuery(GET_USUARIOS);
 
-    useEffect(() => {
-        console.log('data_servidor', data);
-    }, [data]);
+  useEffect(() => {
+    if (error) {
+      toast.error('Error consultando los usuarios');
+    }
+  }, [error]);
 
-    return <div>Todos los usuarios</div>
+  if (loading) return <div>Cargando....</div>;
+
+  return (
+    //<PrivateRoute roleList={['ADMINISTRADOR']}>
+      <div>
+        Datos Usuarios:
+        <table className='tabla'>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellidos</th>
+              <th>Correo</th>
+              <th>Identificación</th>
+              <th>Rol</th>
+              <th>Estado</th>
+              <th>Editar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data && 
+                data.Usuarios.map((u) => {
+                  return(
+                  <tr key={u._id}>
+                    <td>{u.nombre}</td>
+                    <td>{u.apellido}</td>
+                    <td>{u.correo}</td>
+                    <td>{u.identificacion}</td>
+                    {/*<td>{Enum_Rol[u.rol]}</td>
+                    <td>{Enum_EstadoUsuario[u.estado]}</td>*/}
+                    {/*<td>
+                      <Link to={`/usuarios/editar/${u._id}`}>
+                        <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
+                      </Link>
+                    </td>*/}
+                  </tr>
+                );
+
+                })}
+          </tbody>
+        </table>
+      </div>
+    //</PrivateRoute>
+  );
 };
 
 export default IndexUsuarios;
--- src/pages/usuarios/editar.jsx
+++ src/pages/usuarios/editar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {useParams, Link} from 'react-router-dom';
+
 
 const EditarUsuario = () => {
+
+    const {_id} = useParams();
     return (
-        <div> editar usuario</div>
+        <div> editar usuario{_id}</div>
     )
 }
 
