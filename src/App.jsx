2dac13547c53799943bb394f9498a71ce395f770
sebastianmora1618 <jeisonmora1618@gmail.com>
2021-12-20 21:13:55 -0500

    heroku

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ import jwt_decode from 'jwt-decode';
 // import PrivateRoute from 'components/PrivateRoute';
 
 const httpLink = createHttpLink({
-  // uri: 'https://stairs-proyect.herokuapp.com/graphql',
-  uri: 'https://stairs-proyect.herokuapp.com/graphql',
+  // uri: 'https://servidor-gql-mintic.herokuapp.com/graphql',
+  uri: 'https://servidor-gql-mintic.herokuapp.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
